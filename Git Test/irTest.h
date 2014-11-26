bbbee46bdd3234cbb740e82b6837352d28cec98e
ronald chavez <romichama@ronald-chavezs-MacBook-Pro.local>
2014-11-26 08:56:21 -0500

    added a test class

---
+++ Git Test/irTest.h
@@ -0,0 +1,13 @@
+//
+//  irTest.h
+//  Git Test
+//
+//  Created by ronald chavez on 11/26/14.
+//  Copyright (c) 2014 iuvos. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface irTest : NSObject
+
+@end
---
+++ Git Test/irTest.m
@@ -0,0 +1,13 @@
+//
+//  irTest.m
+//  Git Test
+//
+//  Created by ronald chavez on 11/26/14.
+//  Copyright (c) 2014 iuvos. All rights reserved.
+//
+
+#import "irTest.h"
+
+@implementation irTest
+
+@end
